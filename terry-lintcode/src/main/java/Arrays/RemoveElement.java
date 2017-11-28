1b1e0df66f0c8ddc0d250ea523cfaf2571b668bb
txing <txing@walmartlabs.com>
2017-11-28 15:42:08 -0800

    no message

---
+++ terry-lintcode/src/main/java/Arrays/RemoveElement.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+import java.lang.String; /**
+ * Created by IntelliJ IDEA.
+ * User  :  txing
+ * Data  :  11/28/17
+ * Time  :  3:30 PM
+ */
+public class RemoveElement {
+
+//
+//    Given an array and a value, remove all occurrences of that value in place and return the new length.
+//    The order of elements can be changed, and the elements after the new length don't matter.
+
+
+    public int removeElement(int[] A, int elem) {
+
+        if(A==null || A.length ==0){
+            return 0;
+        }
+
+        int i=0;
+        int j;
+
+        for(j=0; j<A.length; j++){
+            if(A[j]!=elem){
+                A[i]=A[j];
+                i++;
+            }
+        }
+
+        return i;
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
