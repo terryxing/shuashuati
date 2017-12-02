69239b488575e206957d2b0b470595593ac81cc1
txing <txing@walmartlabs.com>
2017-12-01 16:18:23 -0800

    add more

---
+++ terry-lintcode/src/main/java/BinarySearch/WoodCut.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User  :  txing
+ * Data  :  12/1/17
+ * Time  :  4:13 PM
+ */
+public class WoodCut {
+
+    public int woodCut(int[] L, int k) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
