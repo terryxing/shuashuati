fa71a058bbfb9585b1e7051765368e0c7bfd9e02
xingtianyi <xingtianyi@gmail.com>
2017-12-25 16:48:43 -0800

    add more

---
+++ terry-lintcode/src/main/java/SearchRecursion/CombinationSum.java
@@ -0,0 +1,59 @@
+package SearchRecursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by xingtianyi on 12/25/17.
+ */
+public class CombinationSum {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // write your code here
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if(candidates.length == 0){
+            return result;
+        }
+
+        int len = candidates.length;
+
+        List<Integer> levelResult = new ArrayList<Integer>();
+
+        Arrays.sort(candidates);
+        Set<List<Integer>> set = new HashSet<List<Integer>>();
+
+        helper(0, 0, target, candidates, levelResult, result, set, len);
+
+        return result;
+
+    }
+
+
+    private void helper(int index, int sum, int target, int[] candidates, List<Integer> levelResult, List<List<Integer>> result, Set<List<Integer>> set, int len){
+
+        if(sum > target){
+            return;
+        }
+
+        if(sum == target){
+            if(!set.contains(levelResult)){
+                result.add(new ArrayList<Integer>(levelResult));
+                set.add(levelResult);
+            }
+            return;
+        }
+
+        for(int i=index; i<len; i++){
+            sum += candidates[i];
+            levelResult.add(candidates[i]);
+            helper(i, sum, target, candidates, levelResult, result, set, len);
+            sum = sum-candidates[i];
+            levelResult.remove(levelResult.size()-1);
+        }
+    }
+}
---
+++ terry-lintcode/src/main/java/SearchRecursion/Combinations.java
@@ -0,0 +1,44 @@
+package SearchRecursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xingtianyi on 12/25/17.
+ */
+public class Combinations {
+
+    public List<List<Integer>> combine(int n, int k) {
+        // write your code here
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if(k>n){
+            return result;
+        }
+
+        List<Integer> levelResult = new ArrayList<Integer>();
+
+        helper(1, n, k, result, levelResult, 0);
+
+        return result;
+
+    }
+
+    private void helper(int i, int n, int k, List<List<Integer>> result, List<Integer> levelResult, int count){
+
+        if(count==k){
+            result.add(new ArrayList<Integer>(levelResult));
+            return;
+        }
+
+        for(int x=i; x<=n; x++){
+            levelResult.add(x);
+            count++;
+            helper(x+1, n, k, result, levelResult, count);
+            levelResult.remove(levelResult.size()-1);
+            count--;
+        }
+    }
+
+}
