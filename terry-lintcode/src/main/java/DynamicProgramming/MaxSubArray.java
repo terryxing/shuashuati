9412c3117473a3111ac6dcd2c89a7c372dc51748
txing <txing@walmartlabs.com>
2017-12-26 19:48:33 -0800

    add more

---
+++ terry-lintcode/src/main/java/DynamicProgramming/MaxSubArray.java
@@ -0,0 +1,38 @@
+package DynamicProgramming;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User  :  txing
+ * Data  :  12/26/17
+ * Time  :  7:48 PM
+ */
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        // write your code
+
+        if(nums == null || nums.length == 0 ){
+            return 0;
+        }
+
+        int len = nums.length;
+
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for(int i=0; i<len; i++){
+            sum +=nums[i];
+
+            if (sum>max){
+                max = sum;
+            }
+
+            if(sum < 0){
+                sum = 0;
+            }
+
+        }
+        return max;
+    }
+
+}
