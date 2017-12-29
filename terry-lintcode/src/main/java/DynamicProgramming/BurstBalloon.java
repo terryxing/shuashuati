eeae4d59cf364dfc838e7eb15b9e4766319db5c6
txing <txing@walmartlabs.com>
2017-12-28 21:52:25 -0800

    add more

--- terry-lintcode/src/main/java/DynamicProgramming/BurstBalloon.java
+++ terry-lintcode/src/main/java/DynamicProgramming/BurstBalloon.java
@@ -45,6 +45,9 @@ public class BurstBalloon {
         }
 
         for(int k=i; k<=j; k++){
+            //为什么是cost k是k乘以i-1 j+1，因为k-1 k+1已经在search左边区间和search右边区间乘过了，他的意思是，以K
+            //为分割线，左边的最优解那就是已经所有都爆掉了，右边的也是，既然都已经爆掉了，k就无法乘K-1 K+1
+
             dp[i][j] = Math.max(dp[i][j], search(nums, i, k-1, dp) + search(nums, k+1, j, dp) + cost(k, nums, i, j));
         }
         return dp[i][j];
